7675d11829b502b18fb7ffa3ea9178c0db1f2ef8
Fr33zyy <lois.giraudon@epita.fr>
2024-06-12 20:24:11 +0200

    projectile

---
+++ Projet S2/Assets/character/Projectile.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Projectile : MonoBehaviour
+{
+    public float projectileSpeed = 1;
+    public GameObject projectile;
+    public Transform firePoint;
+    private Transform playerTransform;
+    private float timefire = 2f;
+    private float firerate = 4;
+
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time > timefire)
+        {
+            timefire = Time.time + 1/firerate;
+            ShootProjectile();
+        }
+    }
+
+    void ShootProjectile()
+    {
+        Vector3 playerPosition = playerTransform.position;
+    Vector3 playerForward = playerTransform.forward;
+
+    // Décaler la position de spawn pour qu'elle soit juste devant le joueur
+    Vector3 spawnPosition = playerPosition + playerForward * 2f + Vector3.up; // Vector3.up pour déplacer légèrement le projectile au-dessus du sol
+
+    Quaternion spawnRotation = firePoint.rotation;
+
+    // Instancier le projectile à la position calculée
+    var projectileObj = Instantiate(projectile, spawnPosition, spawnRotation) as GameObject;
+
+    // Appliquer la vitesse au projectile dans la direction du joueur
+    projectileObj.GetComponent<Rigidbody>().velocity = playerForward * projectileSpeed;
+    }
+}
---
+++ Projet S2/Assets/character/bouledefeu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouledefeu : MonoBehaviour
+{
+    
+    public GameObject impactVFX;
+    private bool collided;
+    void OnCollisionEnter(Collision co)
+    {
+        if (co.gameObject.tag != "Bullet" && co.gameObject.tag != "Player" && !collided)
+        {
+            collided = true;
+
+            var impact = Instantiate(impactVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+            Destroy(impact, 2);
+            Destroy (gameObject);
+        }
+        
+    }
+}
